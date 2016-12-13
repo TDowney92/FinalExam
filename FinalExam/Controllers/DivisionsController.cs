9a7a92600d7efbb98a92f36da82d052db876b4b8
TDowney92 <tmed425@hotmail.com>
2016-12-13 09:20:05 -0500

    doing stuff

---
+++ FinalExam/Controllers/DivisionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalExam.Controllers
+{
+    public class DivisionsController : Controller
+    {
+        // GET: Divisions
+        public String Index()
+        {
+            return "Divisions()";
+        }
+    }
+}
