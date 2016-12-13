1f9ff472ba9cdd86cc87b66f1c4bf3b78cd06c79
TDowney92 <tmed425@hotmail.com>
2016-12-13 09:55:39 -0500

    more exam stuff

---
+++ FinalExam/Models/Teams.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalExam.Models
+{
+    [Table("Teams")]
+    public partial class Teams 
+    {
+        // GET api/<controller>
+        public Teams()
+        {
+            Teams = new HashSet<Teams>();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
